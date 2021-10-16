1e3bedf94fc5a0057062fa0d71740d3c0f3d8d50
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-16 16:35:44 +0200

    feat(Domain): add IProductRepository interface

---
+++ Domain/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseCore.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAll();
+        Task<List<Product>> GetAvailable();
+        Task<List<Product>> GetSold();
+        Task<Product> GetById();
+        Task<bool> Add(Product product);
+        Task<bool> Update(Product product);
+        Task<bool> ChangeStatus(int id);
+    }
+}
