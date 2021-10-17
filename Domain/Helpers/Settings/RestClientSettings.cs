2ccb313c6102c39421bf0b827e3bc648132fc36c
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-17 13:10:57 +0200

    feat(Domain): add RestClientSettings class

---
+++ Domain/Helpers/Settings/RestClientSettings.cs
@@ -0,0 +1,13 @@
+﻿using RestSharp;
+using System.Collections.Generic;
+
+namespace Domain.Helpers.Settings
+{
+    public class RestClientSettings
+    {
+        public string Endpoint { get; set; }
+        public Method Method { get; set; }
+        public object Payload { get; set; }
+        public Dictionary<string, string> QueryStringParameters { get; set; }
+    }
+}
