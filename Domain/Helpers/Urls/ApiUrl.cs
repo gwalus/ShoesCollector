506ea8bda29f27299129ac0f1756f8c49075e9bf
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-17 13:12:13 +0200

    feat(Domain): add ApiUrl class to store endpoint names

---
+++ Domain/Helpers/Urls/ApiUrl.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Helpers.Urls
+{
+    public static class ApiUrl
+    {
+        public const string BaseApiUrl = "http://localhost:5000/api/";
+
+        public const string Products = "products";
+    }
+}
