4bddaa810e2509f6e5662a430f3fee4ed610210d
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-17 13:11:24 +0200

    feat(Domain): add IBaseRestClient interface

---
+++ Domain/Interfaces/Clients/IBaseRestClient.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Helpers.Settings;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Clients
+{
+    public interface IBaseRestClient
+    {
+        Task CallAsync(RestClientSettings clientSettings);
+        Task<T> CallAsync<T>(RestClientSettings clientSettings) where T : new();
+        string GetUrl(string endpoint);
+    }
+}
