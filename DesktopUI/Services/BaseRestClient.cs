7d154fceb5d3878d0a49fdd71a604fad79f22ef8
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-17 13:13:01 +0200

    feat(DesktopUI): implementation of BaseRestClient

---
+++ DesktopUI/Services/BaseRestClient.cs
@@ -0,0 +1,59 @@
+﻿using Domain.Helpers.Settings;
+using Domain.Helpers.Urls;
+using Domain.Interfaces.Clients;
+using RestSharp;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Services
+{
+    public class BaseRestClient : IBaseRestClient
+    {
+        private readonly IRestClient _restClient;
+
+        public BaseRestClient(IRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        public async Task CallAsync(RestClientSettings clientSettings)
+        {
+            await CallAsync<object>(clientSettings);
+        }
+
+        public async Task<T> CallAsync<T>(RestClientSettings clientSettings) where T : new()
+        {
+            clientSettings.Endpoint = GetUrl(clientSettings.Endpoint);
+
+            var request = GetRequest(clientSettings);
+            var response = await BaseCallAsync<T>(request);
+            
+            return response.Data;
+        }
+
+        public string GetUrl(string endpoint)
+        {
+            return ApiUrl.BaseApiUrl + endpoint;
+        }
+
+        private static RestRequest GetRequest(RestClientSettings clientSettings)
+        {
+            RestRequest request = new(clientSettings.Endpoint, clientSettings.Method);
+
+            if (clientSettings.QueryStringParameters?.Any() == true)
+            {
+                foreach (var parameter in clientSettings.QueryStringParameters)
+                {
+                    request.AddParameter(parameter.Key, parameter.Value, ParameterType.QueryString);
+                }
+            }
+
+            if (clientSettings.Payload != null)
+                request.AddJsonBody(clientSettings.Payload);
+
+            return request;
+        }
+
+        private async Task<IRestResponse<T>> BaseCallAsync<T>(IRestRequest restRequest) where T : new() => await _restClient.ExecuteAsync<T>(restRequest);
+    }
+}
