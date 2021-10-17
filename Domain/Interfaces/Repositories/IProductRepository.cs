04a558b68f5f8f1f3d595f769001206d306f23b4
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-17 16:28:58 +0200

    feat(Domain): add products filter

--- Domain/Interfaces/Repositories/IProductRepository.cs
+++ Domain/Interfaces/Repositories/IProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Helpers.Filters;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,9 +7,7 @@ namespace Domain.Interfaces.Repositories
 {
     public interface IProductRepository
     {
-        Task<List<Product>> GetAll();
-        Task<List<Product>> GetAvailable();
-        Task<List<Product>> GetSold();
+        Task<List<Product>> GetAll(ProductFilter productFilter);
         Task<Product> GetById();
         Task<bool> Add(Product product);
         Task<bool> Update(Product product);
