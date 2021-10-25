33ffbc2f9740542c08a5f2c0e651bbe13805b6a0
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-25 18:01:07 +0200

    feat(WebAPI): add GetFirstFurchase endpoint in statistics controller

---
+++ WebAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IProductStatisticsRepository _productStatisticsRepository;
+        private readonly IProductDatesStatisticsRepository _productDatesStatisticsRepository;
+        private readonly IProductPricesStatisticsRepository _productPricesStatisticsRepository;
+
+        public StatisticsController(IProductStatisticsRepository productStatisticsRepository, IProductDatesStatisticsRepository productDatesStatisticsRepository, IProductPricesStatisticsRepository productPricesStatisticsRepository)
+        {
+            _productStatisticsRepository = productStatisticsRepository;
+            _productDatesStatisticsRepository = productDatesStatisticsRepository;
+            _productPricesStatisticsRepository = productPricesStatisticsRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Product>> GetFirstFurchase()
+        {
+            return Ok(await _productStatisticsRepository.GetFirstPurchaseAsync());
+        }
+    }
+}
