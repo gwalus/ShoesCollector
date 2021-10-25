84cccd71967fd69a795a2794c0a9bb1317343fce
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-25 17:36:48 +0200

    feat(Domain): add IProductDatesStatisticsRepository interface

---
+++ Domain/Interfaces/Repositories/IProductDatesStatisticsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IProductDatesStatisticsRepository
+    {
+        Task<int> GetDaysOfFirstPurchase();
+        Task<int> GetDaysOfLatestPurchase();
+        Task<int> GetDaysOfLatestSale();
+    }
+}
