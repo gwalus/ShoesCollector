14b6e327845aa33384a6c3d4ee3aeeee0919227e
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-25 17:35:14 +0200

    feat(Domain): add IProductStatisticsRepository interface

---
+++ Domain/Interfaces/Repositories/IProductStatisticsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IProductStatisticsRepository
+    {
+        Task<Product> GetFirstPurchase();
+        Task<Product> GetLatestPurchase();
+        Task<Product> GetLatestSale();
+    }
+}
