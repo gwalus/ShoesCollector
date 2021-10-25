5a06427eac6b124e1b1087e42d1236e92aab2413
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-25 18:24:43 +0200

    feat(WebAPI): implementation other statistics endpoint

--- WebAPI/Controllers/StatisticsController.cs
+++ WebAPI/Controllers/StatisticsController.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Helpers.Urls;
 using Domain.Interfaces.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -20,22 +21,64 @@ namespace WebAPI.Controllers
             _productPricesStatisticsRepository = productPricesStatisticsRepository;
         }
 
-        [HttpGet]
+        [HttpGet(ApiUrl.FirstPurchase)]
         public async Task<ActionResult<Product>> GetFirstFurchase()
         {
             return Ok(await _productStatisticsRepository.GetFirstPurchaseAsync());
         }
 
-        [HttpGet]
+        [HttpGet(ApiUrl.LatestPurchase)]
         public async Task<ActionResult<Product>> GetLatestPurchase()
         {
             return Ok(await _productStatisticsRepository.GetLatestPurchaseAsync());
         }
 
-        [HttpGet]
+        [HttpGet(ApiUrl.LatestSale)]
         public async Task<ActionResult<Product>> GetLatestSale()
         {
             return Ok(await _productStatisticsRepository.GetLatestSaleAsync());
+        }
+
+        [HttpGet(ApiUrl.DaysOfFirstPurchase)]
+        public async Task<ActionResult<Product>> GetDaysOfFirstPurchase()
+        {
+            return Ok(await _productDatesStatisticsRepository.GetDaysOfFirstPurchaseAsync());
+        }
+
+        [HttpGet(ApiUrl.DaysOfLatestPurchase)]
+        public async Task<ActionResult<Product>> GetDaysOfLatestPurchase()
+        {
+            return Ok(await _productDatesStatisticsRepository.GetDaysOfLatestPurchaseAsync());
+        }
+
+        [HttpGet(ApiUrl.DaysOfLatestSale)]
+        public async Task<ActionResult<Product>> GetDaysOfLatestSale()
+        {
+            return Ok(await _productDatesStatisticsRepository.GetDaysOfLatestSaleAsync());
+        }
+
+        [HttpGet(ApiUrl.BestProfit)]
+        public async Task<ActionResult<Product>> GetBestProfit()
+        {
+            return Ok(await _productPricesStatisticsRepository.GetBestProfitAsync());
+        }
+
+        [HttpGet(ApiUrl.LowestProfit)]
+        public async Task<ActionResult<Product>> GetLowestProfit()
+        {
+            return Ok(await _productPricesStatisticsRepository.GetLowestProfitAsync());
+        }
+
+        [HttpGet(ApiUrl.BiggestPurchase)]
+        public async Task<ActionResult<Product>> GetBiggestPurchase()
+        {
+            return Ok(await _productPricesStatisticsRepository.GetBiggestPurchaseAsync());
+        }
+
+        [HttpGet(ApiUrl.LowestPurchase)]
+        public async Task<ActionResult<Product>> GetLowestPurchase()
+        {
+            return Ok(await _productPricesStatisticsRepository.GetLowestPurchaseAsync());
         }
     }
 }
