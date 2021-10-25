c2732e1f9fa9fb23de074031eeefa90023251ab2
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-25 17:39:47 +0200

    feat(Domain): add IProductPricesStatisticsRepository interface

---
+++ Domain/Interfaces/Repositories/IProductPricesStatisticsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IProductPricesStatisticsRepository
+    {
+        Task<double> GetBestProfit();
+        Task<double> GetLowestProfit();
+        Task<double> GetBiggestPurchase();
+        Task<double> GetLowestPurchase();
+    }
+}
