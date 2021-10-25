f68814d10b619e3274f455ae81a0286bf119efb2
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-25 19:16:30 +0200

    feat(DatabaseCore): implementation GetTotalsByFilterAsync method

---
+++ DatabaseCore/Repositories/TotalsStatisticsRepository.cs
@@ -0,0 +1,25 @@
+﻿using DatabaseCore.DataContext;
+using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DatabaseCore.Repositories
+{
+    public class TotalsStatisticsRepository : ITotalsStatisticsRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public TotalsStatisticsRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<double> GetTotalsByFilterAsync(Expression<Func<Product, double>> func)
+        {
+            return await _dbContext.Products.SumAsync(func);
+        }
+    }
+}
