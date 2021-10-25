774c782427a443d65e4cebd72c7abdfc090cb6e3
Grzegorz Waluś <gwalus1404@gmail.com>
2021-10-25 19:15:40 +0200

    feat(Domain): add ITotalsStatisticsRepository interface

---
+++ Domain/Interfaces/Repositories/ITotalsStatisticsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface ITotalsStatisticsRepository
+    {
+        Task<double> GetTotalsByFilterAsync(Expression<Func<Product, double>> func);
+    }
+}
