b9e4d8e3e3790d8f5efa1d6f4e92c8ebc70e1115
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-05 19:39:14 +0100

    feat(DesktopUI): add SearchProductCommand

---
+++ DesktopUI/Commands/SearchProductCommand.cs
@@ -0,0 +1,32 @@
+﻿using DesktopUI.ViewModels;
+using System;
+using System.Windows.Input;
+using Prism.Ioc;
+using DesktopUI.Interfaces;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Commands
+{
+    public class SearchProductCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var productService = ContainerLocator.Container.Resolve<IProductService>();
+            ProductSearchFilterViewModel filter = null;
+
+            if (parameter != null)
+                filter = parameter as ProductSearchFilterViewModel;
+
+            var filteredProducts = Task.Run(async () => await productService.GetProducts(filter)).Result;
+
+            productService.SetProductView(filteredProducts);
+        }
+    }
+}
