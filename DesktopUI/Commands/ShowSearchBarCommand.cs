7b6b921918184c70b69b5aa206c12db1d14878ae
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-05 19:37:33 +0100

    feat(DesktopUI): add ShowSearchBarCommand

---
+++ DesktopUI/Commands/ShowSearchBarCommand.cs
@@ -0,0 +1,31 @@
+﻿using DesktopUI.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DesktopUI.Commands
+{
+    public class ShowSearchBarCommand : ICommand
+    {
+        private readonly ProductsViewViewModel _viewModel;
+
+        public ShowSearchBarCommand(ProductsViewViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_viewModel.SearchBarVisibilityMode == Visibility.Collapsed)
+                _viewModel.SearchBarVisibilityMode = Visibility.Visible;
+            else _viewModel.SearchBarVisibilityMode = Visibility.Collapsed;
+        }
+    }
+}
