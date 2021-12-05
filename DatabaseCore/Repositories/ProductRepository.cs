f67e756df8982d7b2b9355ad394f1ef4579f0f64
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-05 16:12:45 +0100

    feat(DatabaseCore): add product query filters in GetAll method

--- DatabaseCore/Repositories/ProductRepository.cs
+++ DatabaseCore/Repositories/ProductRepository.cs
@@ -37,12 +37,35 @@ namespace DatabaseCore.Repositories
             switch (productFilter.Condition)
             {
                 case nameof(ProductFilterEnum.Available):
-                    return await query.Where(x => !x.IsSold).ToListAsync();
+                    query = query.Where(x => !x.IsSold);
+                    break;
                 case nameof(ProductFilterEnum.Sold):
-                    return await query.Where(x => x.IsSold).ToListAsync();
-                default:
-                    return await query.ToListAsync();
+                    query = query.Where(x => x.IsSold);
+                    break;
             }
+
+            if (!string.IsNullOrEmpty(productFilter.ProductName))
+                query = query.Where(x => x.Name.ToLower().Contains(productFilter.ProductName.ToLower()));
+
+            if (!string.IsNullOrEmpty(productFilter.Brand))
+                query = query.Where(x => x.Brand.ToLower().Contains(productFilter.Brand.ToLower()));
+
+            if (!string.IsNullOrEmpty(productFilter.Code))
+                query = query.Where(x => x.ProductCode.ToLower().Contains(productFilter.Code.ToLower()));
+
+            if (!string.IsNullOrEmpty(productFilter.Color))
+                query = query.Where(x => x.Color.ToLower().Contains(productFilter.Color.ToLower()));
+
+            if (!string.IsNullOrEmpty(productFilter.Size))
+                query = query.Where(x => x.Size.ToLower().Contains(productFilter.Size.ToLower()));
+
+            if (!string.IsNullOrEmpty(productFilter.Source))
+                query = query.Where(x => x.Source.ToLower().Contains(productFilter.Source.ToLower()));
+
+            if (productFilter.Box.HasValue)
+                query = query.Where(x => x.Box.Value == productFilter.Box);
+
+            return await query.ToListAsync();            
         }
 
         public Task<Product> GetById()
