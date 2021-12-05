0526a4b09bf25e3efaae062d7d1ee3b3b0e61a71
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-05 14:31:12 +0100

    feat(DesktopUI): add ProductTotalViewModel to show totals view

---
+++ DesktopUI/ViewModels/ProductTotalViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Prism.Mvvm;
+
+namespace DesktopUI.ViewModels
+{
+    public class ProductTotalViewModel : BindableBase
+    {
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set { SetProperty(ref _description, value); }
+        }
+
+        private int _quantityTotal;
+        public int QuantityTotal
+        {
+            get { return _quantityTotal; }
+            set { SetProperty(ref _quantityTotal, value); }
+        }
+
+        private double _purchaseTotal;
+        public double PurchaseTotal
+        {
+            get { return _purchaseTotal; }
+            set { SetProperty(ref _purchaseTotal, value); }
+        }
+
+        private double _sellTotal;
+        public double SellTotal
+        {
+            get { return _sellTotal; }
+            set { SetProperty(ref _sellTotal, value); }
+        }
+
+        private double _shipTotal;
+        public double ShipTotal
+        {
+            get { return _shipTotal; }
+            set { SetProperty(ref _shipTotal, value); }
+        }
+
+        private double _withoutShipTotal;
+        public double WithoutShipTotal
+        {
+            get { return _withoutShipTotal; }
+            set { SetProperty(ref _withoutShipTotal, value); }
+        }
+
+        private double _profitTotal;
+        public double ProfitTotal
+        {
+            get { return _profitTotal; }
+            set { SetProperty(ref _profitTotal, value); }
+        }
+    }
+}
