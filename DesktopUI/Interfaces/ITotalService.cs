6b8566d1c4e541641db535dfd41c1af4ea85ac16
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-05 14:34:58 +0100

    feat(DesktopUI): add ITotalService interface

---
+++ DesktopUI/Interfaces/ITotalService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace DesktopUI.Interfaces
+{
+    public interface ITotalService
+    {
+        int GetTotalCount(bool? isSold = null);
+        Task<double> GetTotalPurchase(bool? isSold = null);
+        Task<double> GetTotalSell(bool? isSold = null);
+        Task<double> GetTotalShip(bool? isSold = null);
+        Task<double> GetTotalWithoutShip(bool? isSold = null);
+        Task<double> GetTotalProfit(bool? isSold = null);
+        void SetProductTotalsView(bool? isSold = null, string description = null);
+    }
+}
