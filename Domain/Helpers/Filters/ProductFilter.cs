b1b69473c8701a07ae6fc0386eedc0dc014c5df0
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-05 16:11:34 +0100

    feat(Domain): add properties to product filtering

--- Domain/Helpers/Filters/ProductFilter.cs
+++ Domain/Helpers/Filters/ProductFilter.cs
@@ -3,5 +3,12 @@
     public class ProductFilter
     {
         public string Condition { get; set; }
+        public string Brand { get; set; }
+        public string ProductName { get; set; }
+        public string Code { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public bool? Box { get; set; } = null;
+        public string Source { get; set; }
     }
 }
