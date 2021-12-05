2898caded8e2aa78f93a4550787c51d2f91ca34b
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-05 19:38:14 +0100

    feat(DesktopUI): add IProductService interface

---
+++ DesktopUI/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using DesktopUI.ViewModels;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Interfaces
+{
+    public interface IProductService
+    {
+        Task<List<Product>> GetProducts(ProductSearchFilterViewModel productSearchFilter);
+        void SetProductView(List<Product> products);
+    }
+}
