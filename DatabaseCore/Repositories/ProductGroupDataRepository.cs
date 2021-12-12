6a5ea5e4e609169772aeb0b3dddd5c48b5fed6e1
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-12 18:59:51 +0100

    feat(DatabaseCore): implement GetLossProductGroupData in ProductGroupDataRepository

--- DatabaseCore/Repositories/ProductGroupDataRepository.cs
+++ DatabaseCore/Repositories/ProductGroupDataRepository.cs
@@ -21,7 +21,31 @@ namespace DatabaseCore.Repositories
 
         public async Task<List<ProductGroupData>> GetLossProductGroupData()
         {
-            throw new NotImplementedException();
+            var products = await _dbContext.Products.ToListAsync();
+
+            var groupedSoldLossProducts = products
+                .Where(x => x.IsSold && x.Profit < 0)
+                .OrderByDescending(x => DateTime.Parse(x.SaleDate))
+                .GroupBy(x => new { DateTime.Parse(x.SaleDate).Year, DateTime.Parse(x.SaleDate).Month })
+                .ToList();
+
+            var productGroupDatas = new List<ProductGroupData>();
+
+            foreach (var item in groupedSoldLossProducts)
+            {
+                var groupedData = new ProductGroupData()
+                {
+                    Year = item.Key.Year,
+                    Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(item.Key.Month),
+                    Count = item.Count(),
+                    Purchase = Math.Round(item.Sum(x => x.Profit.Value), 2),
+                    Average = Math.Round((item.Sum(x => x.Profit.Value) / item.Count()), 2)
+                };
+
+                productGroupDatas.Add(groupedData);
+            }
+
+            return productGroupDatas;
         }
 
         public async Task<List<ProductGroupData>> GetPurchaseProductGroupData()
