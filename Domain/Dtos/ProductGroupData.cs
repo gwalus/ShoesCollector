d8f3f92b5fc15d8071dcd5424a8dced7601eb1c2
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-12 17:32:22 +0100

    feat(Domain): add ProductGroupData data model

---
+++ Domain/Dtos/ProductGroupData.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Dtos
+{
+    public class ProductGroupData
+    {
+        public int Year { get; set; }
+        public string Month { get; set; }
+        public int Count { get; set; }
+        public double Purchase { get; set; }
+        public double Profit { get; set; }
+        public double Average { get; set; }
+    }
+}
