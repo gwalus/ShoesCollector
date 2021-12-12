e8912238ecaade01ab68ea332ee24d77480c9679
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-12 17:32:47 +0100

    feat(Domain): add IProductGroupDataRepository interface

---
+++ Domain/Interfaces/Repositories/IProductGroupDataRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Dtos;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IProductGroupDataRepository
+    {
+        Task<ProductGroupData> GetSoldProductGroupData();
+        Task<ProductGroupData> GetPurhcaseProductGroupData();
+        Task<ProductGroupData> GetLossProductGroupData();
+    }
+}
