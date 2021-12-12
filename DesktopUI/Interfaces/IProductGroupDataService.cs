ecbb944a8aa01a4ef2dcdbb420f5077c84f0eafe
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-12 18:25:05 +0100

    feat(DesktopUI): add IProductGroupDataService interface

---
+++ DesktopUI/Interfaces/IProductGroupDataService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Interfaces
+{
+    public interface IProductGroupDataService
+    {
+        Task<List<ProductGroupData>> GetProductSoldGroupData();
+        Task<List<ProductGroupData>> GetProductPurchaseGroupData();
+        Task<List<ProductGroupData>> GetProductLossGroupData();
+    }
+}
