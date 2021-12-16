d0f5d6d37f3b99b4961341697fe609f59db1579e
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-16 23:21:41 +0100

    feat(WebAPI): add endpoint to add new product

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,10 +1,12 @@
-﻿using Domain.Entities;
+﻿using AutoMapper;
+using Domain.Entities;
 using Domain.Helpers.Filters;
 using Domain.Helpers.Urls;
 using Domain.Interfaces.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using WebAPI.Dtos;
 
 namespace WebAPI.Controllers
 {
@@ -12,16 +14,29 @@ namespace WebAPI.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductRepository productRepository)
+        public ProductsController(IProductRepository productRepository, IMapper mapper)
         {
             _productRepository = productRepository;
+            _mapper = mapper;
         }
 
         [HttpGet(ApiUrl.Products)]
         public async Task<ActionResult<IEnumerable<Product>>> GetProductsAsync([FromQuery]ProductFilter productFilter)
         {
-            return Ok(await _productRepository.GetAll(productFilter));
+            return Ok(await _productRepository.GetAllAsync(productFilter));
+        }
+
+        [HttpPost(ApiUrl.Product)]
+        public async Task<ActionResult> AddProduct([FromBody] ProductAddDto productAddDto)
+        {
+            var productToAdd = _mapper.Map<Product>(productAddDto);
+
+            if (await _productRepository.AddAsync(productToAdd))
+                return Ok("Product has been added");
+
+            return BadRequest();
         }
     }
 }
