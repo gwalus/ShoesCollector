7cc8686e21b057db9ed6442a88013a38ef10c15f
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 19:57:40 +0100

    feat(DatabaseCore): implementation IProductSourceRepository

---
+++ DatabaseCore/Repositories/ProductSourceRepository.cs
@@ -0,0 +1,30 @@
+﻿using DatabaseCore.DataContext;
+using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DatabaseCore.Repositories
+{
+    public class ProductSourceRepository : IProductSourceRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductSourceRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> AddProductSourceAsync(ProductSource productSource)
+        {
+            await _dbContext.ProductSources.AddRangeAsync(productSource);
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
+
+        public async Task<List<ProductSource>> GetProductSourcesAsync()
+        {
+            return await _dbContext.ProductSources.ToListAsync();
+        }
+    }
+}
