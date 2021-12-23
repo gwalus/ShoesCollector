21137f5461e23d49135c49689256ae48a77bca12
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 19:07:08 +0100

    feat(WebAPI): add brand controller with basic endpoints

---
+++ WebAPI/Controllers/BrandController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Domain.Helpers.Urls;
+using Domain.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAPI.Dtos;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public BrandController(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet(ApiUrl.Brands)]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            return Ok(await _brandRepository.GetBrandsAsync());
+        }
+
+        [HttpPost(ApiUrl.Brands)]
+        public async Task<ActionResult> AddBrand(BrandToAddDto brandToAddDto)
+        {
+            var brandToAdd = _mapper.Map<Brand>(brandToAddDto);
+
+            return Ok(await _brandRepository.AddBrandAsync(brandToAdd));
+        }
+    }
+}
