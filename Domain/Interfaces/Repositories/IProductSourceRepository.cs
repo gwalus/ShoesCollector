b4e188cf0afc43bc93d39c35228d568afe377e20
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 19:57:03 +0100

    feat(Domain): add IProductSourceRepository

---
+++ Domain/Interfaces/Repositories/IProductSourceRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IProductSourceRepository
+    {
+        Task<bool> AddProductSourceAsync(ProductSource productSource);
+        Task<List<ProductSource>> GetProductSourcesAsync();
+    }
+}
