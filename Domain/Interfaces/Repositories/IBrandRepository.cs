0d94af622e11749eb768ff34dcfbc8c35c223361
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 19:00:59 +0100

    feat(Domain): add IBrandResository

---
+++ Domain/Interfaces/Repositories/IBrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IBrandRepository
+    {
+        Task<bool> AddBrandAsync(Brand brand);
+        Task<List<Brand>> GetBrandsAsync();
+    }
+}
