29cf7fc49f792f13a3795208563a987006aa0b70
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 19:02:12 +0100

    feat(WebAPI): add brand to add DTO

---
+++ WebAPI/Dtos/BrandToAddDto.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Extensions.String;
+
+namespace WebAPI.Dtos
+{
+    public class BrandToAddDto
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name.FirstCharToUpper(); }
+            set { _name = value; }
+        }
+    }
+}
