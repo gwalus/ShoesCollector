669439e5463c375804607cb15c36a04263334ece
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 20:00:53 +0100

    feat(WebAPI): add ProductSourceToAddDto model

---
+++ WebAPI/Dtos/ProductSourceToAddDto.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Extensions.String;
+
+namespace WebAPI.Dtos
+{
+    public class ProductSourceToAddDto
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name.FirstCharToUpper(); }
+            set { _name = value; }
+        }
+    }
+}
