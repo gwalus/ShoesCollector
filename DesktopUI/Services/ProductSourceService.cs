1a23dd6df486124521f56c0fe24d735a92d98819
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 20:26:39 +0100

    feat(DesktopUI): implement get product sources in ProductSourceService

---
+++ DesktopUI/Services/ProductSourceService.cs
@@ -0,0 +1,33 @@
+﻿using DesktopUI.Interfaces;
+using Domain.Entities;
+using Domain.Helpers.Urls;
+using Domain.Interfaces.Clients;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Services
+{
+    public class ProductSourceService : IProductSourceService
+    {
+        private readonly IBaseRestClient _restClient;
+
+        public ProductSourceService(IBaseRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        public Task<bool> AddProductSourceAsync(ProductSource productSource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ProductSource>> GetProductSourcesAsync()
+        {
+            return await _restClient.CallAsync<List<ProductSource>>(new Domain.Helpers.Settings.RestClientSettings
+            {
+                Endpoint = ApiUrl.ProductSources
+            });
+        }
+    }
+}
