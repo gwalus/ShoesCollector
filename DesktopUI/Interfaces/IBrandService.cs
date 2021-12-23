a42c5d0798f10caad93e26a551ca98043f2a1841
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 19:31:06 +0100

    feat(DesktopUI): add IBrandService

---
+++ DesktopUI/Interfaces/IBrandService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Interfaces
+{
+    public interface IBrandService
+    {
+        Task<bool> AddBrandAsync(Brand brand);
+        Task<List<Brand>> GetBrandAsync();
+    }
+}
