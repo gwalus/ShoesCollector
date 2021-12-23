10947b78b61c33075a0b993838c6f75c9c078325
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-23 19:31:34 +0100

    feat(DesktopUI): implement get all brands in Brand service

---
+++ DesktopUI/Services/BrandService.cs
@@ -0,0 +1,33 @@
+﻿using DesktopUI.Interfaces;
+using Domain.Entities;
+using Domain.Helpers.Urls;
+using Domain.Interfaces.Clients;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Services
+{
+    public class BrandService : IBrandService
+    {
+        private readonly IBaseRestClient _restClient;
+
+        public BrandService(IBaseRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        public Task<bool> AddBrandAsync(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Brand>> GetBrandAsync()
+        {
+            return await _restClient.CallAsync<List<Brand>>(new Domain.Helpers.Settings.RestClientSettings
+            {
+                Endpoint = ApiUrl.Brands
+            });
+        }
+    }
+}
