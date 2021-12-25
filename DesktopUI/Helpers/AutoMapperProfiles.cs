fae8ef26923571563b05f38f5d1fda7bd33baff2
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-25 15:57:28 +0100

    feat(DesktopUI): add AutoMapperProfiles for mapping view models to api models

---
+++ DesktopUI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DesktopUI.ViewModelDtos;
+using DesktopUI.ViewModels;
+
+namespace DesktopUI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AddProductViewModel, ProductApiToAdd>()
+                .ForMember(dest => dest.Brand, opt => opt.MapFrom(src => src.SelectedBrand))
+                .ForMember(dest => dest.Source, opt => opt.MapFrom(src => src.SelectedProductSource))
+                .ForMember(dest => dest.DateOfPurchase, opt => opt.MapFrom(src => src.DateOfPurchase.ToShortDateString()))
+                .ForMember(dest => dest.ProductCode, opt => opt.MapFrom(src => src.ProductCode.ToUpper()));
+        }
+    }
+}
