c96ab0dc71a7c418b4a0fbe1c08e2751c1daf631
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-25 16:00:54 +0100

    feat(DesktopUI): add ShowUpdateProductPanelCommand

---
+++ DesktopUI/Commands/ShowUpdateProductPanelCommand.cs
@@ -0,0 +1,31 @@
+﻿using DesktopUI.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DesktopUI.Commands
+{
+    public class ShowUpdateProductPanelCommand : ICommand
+    {
+        private readonly ProductsViewViewModel _viewModel;
+
+        public ShowUpdateProductPanelCommand(ProductsViewViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_viewModel.UpdateProductPanelVisibilityMode == Visibility.Collapsed)
+                _viewModel.UpdateProductPanelVisibilityMode = Visibility.Visible;
+            else _viewModel.UpdateProductPanelVisibilityMode = Visibility.Collapsed;
+        }
+    }
+}
