d9f2f6f0758f55f4099a29c7491c491b8452bded
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-25 16:00:36 +0100

    change(DesktopUI): AddProductCommand

---
+++ DesktopUI/Commands/AddProductCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+using DesktopUI.ViewModels;
+
+namespace DesktopUI.Commands
+{
+    public class AddProductCommand : ICommand
+    {
+        private readonly AddProductViewModel _viewModel;
+
+        public event EventHandler CanExecuteChanged;
+
+        public AddProductCommand(AddProductViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _viewModel.AddProduct(parameter as AddProductViewModel);  
+        }
+    }
+}
