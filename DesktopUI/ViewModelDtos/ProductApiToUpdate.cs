6688e17110ab3a86dc56788e1387c9599ddf39fb
Grzegorz Waluś <gwalus1404@gmail.com>
2021-12-26 21:07:25 +0100

    feat(DesktopUI): add ProductApiToUpdate model

---
+++ DesktopUI/ViewModelDtos/ProductApiToUpdate.cs
@@ -0,0 +1,19 @@
+﻿namespace DesktopUI.ViewModelDtos
+{
+    public class ProductApiToUpdate
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public string ProductCode { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public bool? Box { get; set; } = false;
+        public string Source { get; set; }
+        public string DateOfPurchase { get; set; }
+        public string SaleDate { get; set; }
+        public double PurchasePrice { get; set; }
+        public double? SellingPrice { get; set; } = null;
+        public double? ShippingPrice { get; set; } = null;
+    }
+}
