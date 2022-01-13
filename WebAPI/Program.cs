c8328ed40b1144f15886cfafa576d485052b0f6a
Grzegorz Waluś <gwalus1404@gmail.com>
2022-01-13 23:46:56 +0100

    feat(WebAPI): add config for heroku

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,19 +1,33 @@
+using DatabaseCore.DataContext;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                await context.Database.MigrateAsync();
+                // SEED DATA IF I CAN
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during migration");
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
 using WebAPI.Extensions;
 using WebAPI.Helpers;
 
@@ -13,21 +14,58 @@ namespace WebAPI
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(
                 options =>
-                    options.UseNpgsql(
-                        Configuration.GetConnectionString("DefaultConnection"),
-                        x => x.MigrationsAssembly("DatabaseCore.Migrations")));
+                {
+                    //options.UseNpgsql(
+                    //    Configuration.GetConnectionString("DefaultConnection"),
+                    //    x => x.MigrationsAssembly("DatabaseCore.Migrations")));
+
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                    string connStr;
+
+                    // Depending on if in development or production, use either Heroku-provided
+                    // connection string, or development connection string from env var.
+                    if (env == "Development")
+                    {
+                        // Use connection string from file.
+                        connStr = _configuration.GetConnectionString("DefaultConnection");
+                    }
+                    else
+                    {
+                        // Use connection string provided at runtime by Heroku.
+                        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                        // Parse connection URL to connection string for Npgsql
+                        connUrl = connUrl.Replace("postgres://", string.Empty);
+                        var pgUserPass = connUrl.Split("@")[0];
+                        var pgHostPortDb = connUrl.Split("@")[1];
+                        var pgHostPort = pgHostPortDb.Split("/")[0];
+                        var pgDb = pgHostPortDb.Split("/")[1];
+                        var pgUser = pgUserPass.Split(":")[0];
+                        var pgPass = pgUserPass.Split(":")[1];
+                        var pgHost = pgHostPort.Split(":")[0];
+                        var pgPort = pgHostPort.Split(":")[1];
+
+                        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};sslmode=Prefer;Trust Server Certificate=true";
+                    }
+
+                    // Whether the connection string came from the local development configuration file
+                    // or from the environment variable from Heroku, use it to set up your DbContext.
+                    options.UseNpgsql(connStr);
+                });
+                    
             services.AddApplicationRepositories();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddControllers();
@@ -41,12 +79,12 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
-            }
+            //}
 
             app.UseRouting();
 
