fb7d008db9bcad378abc084bc66305f42dfb71fc
Grzegorz Waluś <gwalus1404@gmail.com>
2022-01-13 23:12:02 +0100

    feat(DesktopUI): change theme mode

--- DesktopUI/Views/MainWindow.xaml.cs
+++ DesktopUI/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MahApps.Metro.Controls;
+﻿using ControlzEx.Theming;
+using MahApps.Metro.Controls;
 
 namespace DesktopUI.Views
 {
@@ -7,9 +8,28 @@ namespace DesktopUI.Views
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        string _mode = "Light.Taupe";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ToggleSwitch_Toggled(object sender, System.Windows.RoutedEventArgs e)
+        {
+            switch (_mode)
+            {
+                case "Light.Taupe":
+                    ThemeManager.Current.ChangeTheme(this, "Dark.Taupe");
+                    _mode = "Dark.Taupe";
+                    break;
+                case "Dark.Taupe":
+                    ThemeManager.Current.ChangeTheme(this, "Light.Taupe");
+                    _mode = "Light.Taupe";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
