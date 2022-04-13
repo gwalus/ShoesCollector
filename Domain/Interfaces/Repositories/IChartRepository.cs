ba79621cce7d20963a4eaef557e9e40e755efcb8
Grzegorz Waluś <gwalus1404@gmail.com>
2022-04-13 22:53:56 +0200

    feat(Domain): add IChartRepository

---
+++ Domain/Interfaces/Repositories/IChartRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IChartRepository
+    {
+        Task<List<ChartBrandSeriesDto>> GetChartBrandSeriesAsync();
+        Task<List<ChartSizeSeriesDto>> GetChartSizeSeriesAsync();
+        Task<List<ChartColorSeriesDto>> GetChartColorSeriesAsync();
+    }
+}
