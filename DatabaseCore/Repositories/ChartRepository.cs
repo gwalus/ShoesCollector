01f53dbe42ace260f99492cbd8a8bcd197964cc8
Grzegorz Waluś <gwalus1404@gmail.com>
2022-04-13 22:55:00 +0200

    feat(DatabaseCore): implementation chart repository

---
+++ DatabaseCore/Repositories/ChartRepository.cs
@@ -0,0 +1,47 @@
+﻿using DatabaseCore.DataContext;
+using Domain.Dtos;
+using Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseCore.Repositories
+{
+    public class ChartRepository : IChartRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ChartRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<ChartBrandSeriesDto>> GetChartBrandSeriesAsync()
+        {
+            return await _dbContext.Products.GroupBy(x => x.Brand).Select(x => new ChartBrandSeriesDto
+            {
+                Name = x.Key,
+                Count = x.Count(),
+            }).ToListAsync();
+        }
+
+        public async Task<List<ChartColorSeriesDto>> GetChartColorSeriesAsync()
+        {
+            return await _dbContext.Products.GroupBy(x => x.Color).Select(x => new ChartColorSeriesDto
+            {
+                Name = x.Key,
+                Count = x.Count(),
+            }).ToListAsync();
+        }
+
+        public async Task<List<ChartSizeSeriesDto>> GetChartSizeSeriesAsync()
+        {
+            return await _dbContext.Products.GroupBy(x => x.Size).Select(x => new ChartSizeSeriesDto
+            {
+                Size = x.Key,
+                Count = x.Count(),
+            }).ToListAsync();
+        }
+    }
+}
